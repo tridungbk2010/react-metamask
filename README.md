3c5d14eca40053f91c85863ecd8270ddc47644a3
Chris Ho <chris.tridung@gmail.com>
2022-04-16 11:46:46 +0700

    init project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-metamask
+
 react metamask
---
+++ src/buttons/Button.tsx
@@ -0,0 +1,10 @@
+import React, { FC, ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+export const Button: FC<Props> = ({ children }) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
